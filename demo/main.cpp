97fa2d07c4035ff48a140caf76c49af1882fc05e
Paul Tsouchlos <developer.paul.123@gmail.com>
2020-04-03 23:25:12 -0400

    Convert Tabs to Spaces

--- demo/main.cpp
+++ demo/main.cpp
@@ -12,81 +12,80 @@ struct dummy_event
 
 struct other_event
 {
-	int id;
-	std::string message;
+    int id;
+    std::string message;
 };
 
 struct third_event
 {
-	double value{ 234.00 };
+    double value{ 234.00 };
 };
 
 void foo(const dummy_event& evt)
 {
-	std::cout << "event: " << evt.message << std::endl;
+    std::cout << "event: " << evt.message << std::endl;
 }
 
 class my_callback_object
 {
 public:
-	my_callback_object() = default;
-	void on_event_fired(const dummy_event& evt) { event_count_++; }
-	void on_third_event() {};
-	[[nodiscard]] int get_event_count() const { return event_count_; }
+    my_callback_object() = default;
+    void on_event_fired(const dummy_event&) { event_count_++; }
+    void on_third_event() {};
+    [[nodiscard]] int get_event_count() const { return event_count_; }
 private:
-	int event_count_{ 0 };
+    int event_count_{ 0 };
 };
 
 class third_event_object
 {
 public:
-	void on_third_event() {};
+    void on_third_event() {};
 };
 
 int main()
 {
-	using namespace dp;
-	event_bus evt_bus;
-	my_callback_object callback_obj;
+    using namespace dp;
+    event_bus evt_bus;
+    my_callback_object callback_obj;
 
-	auto fo = &foo;
-	const auto reg = evt_bus.register_handler<dummy_event>(&foo);
-	const auto third_event_reg = evt_bus.register_handler<third_event>([](const third_event& evt)
-	{
-			std::cout << "my third event handler: " << evt.value << new_line;
-	});
+    const auto reg = evt_bus.register_handler<dummy_event>(&foo);
+    const auto third_event_reg = evt_bus.register_handler<third_event>([](const third_event& evt)
+    {
+            std::cout << "my third event handler: " << evt.value << new_line;
+    });
 
-	const auto empty_event_handler = evt_bus.register_handler<third_event>([]() {std::cout << "I just do stuff when a third_event is fired." << new_line; });
-	
-	dummy_event evt{ "hello from dummy event" };
-	evt_bus.fire_event(&evt);
-	evt_bus.fire_event(third_event{ 13.0 });
-	evt_bus.remove_handler(third_event_reg);
-	evt_bus.fire_event(third_event{ 13.0 });
-	
-	
-	const auto other_event_reg = evt_bus.register_handler<other_event>([](const other_event& evt) {std::cout << "first other event handler says: " << evt.message << std::endl; });
-	const auto other_event_second_reg = evt_bus.register_handler<other_event>([](const other_event& evt){std::cout << "second other event handler says: " << evt.id << " " << evt.message << std::endl;});
-	const auto dmy_evt_first_reg = evt_bus.register_handler<dummy_event>([](const dummy_event& evt) {std::cout << "wow it works!" << std::endl;});
-	const auto dmy_evt_pmr_reg = evt_bus.register_handler<dummy_event>(&callback_obj , &my_callback_object::on_event_fired);
-	const auto thrid_event_reg_pmr = evt_bus.register_handler<third_event>(&callback_obj, &my_callback_object::on_third_event);
+    const auto empty_event_handler = evt_bus.register_handler<third_event>([]() {std::cout << "I just do stuff when a third_event is fired." << new_line; });
+    
+    dummy_event evt{ "hello from dummy event" };
+    evt_bus.fire_event(&evt);
+    evt_bus.fire_event(third_event{ 13.0 });
+    evt_bus.remove_handler(third_event_reg);
+    evt_bus.fire_event(third_event{ 13.0 });
+    
+    
+    const auto other_event_reg = evt_bus.register_handler<other_event>([](const other_event& evt) {std::cout << "first other event handler says: " << evt.message << std::endl; });
+    const auto other_event_second_reg = evt_bus.register_handler<other_event>([](const other_event& evt){std::cout << "second other event handler says: " << evt.id << " " << evt.message << std::endl;});
+    const auto dmy_evt_first_reg = evt_bus.register_handler<dummy_event>([](const dummy_event& evt) {std::cout << "third event handler says: " << evt.message << std::endl;});
+    const auto dmy_evt_pmr_reg = evt_bus.register_handler<dummy_event>(&callback_obj , &my_callback_object::on_event_fired);
+    const auto thrid_event_reg_pmr = evt_bus.register_handler<third_event>(&callback_obj, &my_callback_object::on_third_event);
 
-	// the following does not compile
-	// third_event_object teo;
-	// const auto rg = evt_bus.register_handler<third_event>(&teo, &my_callback_object::on_third_event);
-	
-	other_event other_evt{ 2, "hello there" };
-	dummy_event dmy_event{ "oh boy..." };
-	
-	evt_bus.fire_event(dmy_event);
+    // the following does not compile
+    // third_event_object teo;
+    // const auto rg = evt_bus.register_handler<third_event>(&teo, &my_callback_object::on_third_event);
+    
+    other_event other_evt{ 2, "hello there" };
+    dummy_event dmy_event{ "oh boy..." };
+    
+    evt_bus.fire_event(dmy_event);
 
-	std::cout << "Firing other event\n";
-	evt_bus.fire_event(other_evt);
-	evt_bus.remove_handler(other_event_reg);
-	std::cout << "Firing other event\n";
-	evt_bus.fire_event(other_evt);
-	evt_bus.fire_event(third_event{});
-	
-	std::cout << "callback count: " << callback_obj.get_event_count() << std::endl;
+    std::cout << "Firing other event\n";
+    evt_bus.fire_event(other_evt);
+    evt_bus.remove_handler(other_event_reg);
+    std::cout << "Firing other event\n";
+    evt_bus.fire_event(other_evt);
+    evt_bus.fire_event(third_event{});
+    
+    std::cout << "callback count: " << callback_obj.get_event_count() << std::endl;
     return 0;
 }
--- eventbus/test/event_bus_tests.cpp
+++ eventbus/test/event_bus_tests.cpp
@@ -5,199 +5,200 @@
 
 struct test_event_type
 {
-	int id{ -1 };
-	std::string event_message{ "" };
-	double data_value{ 1.0 };
+    int id{ -1 };
+    std::string event_message{ "" };
+    double data_value{ 1.0 };
 };
 
 inline std::ostream& operator<<(std::ostream& out, const test_event_type &evt)
 {
-	out << "id: " << evt.id << " msg: " << evt.event_message << " data: " << evt.data_value;
-	return out;
+    out << "id: " << evt.id << " msg: " << evt.event_message << " data: " << evt.data_value;
+    return out;
 }
 
 class event_handler_counter
 {
-	std::atomic<unsigned int> event_count_ = 0;
+    std::atomic<unsigned int> event_count_ = 0;
 public:
-	event_handler_counter() = default;
-	[[nodiscard]] unsigned int get_count() const { return event_count_.load(); };
-	void on_test_event()
-	{
-		++event_count_;
-	}
+    event_handler_counter() = default;
+    [[nodiscard]] unsigned int get_count() const { return event_count_.load(); };
+    void on_test_event()
+    {
+        ++event_count_;
+    }
 };
 
 class EventBusTestFixture : public ::testing::Test
 {
 protected:
-	event_handler_counter counter;
-	dp::handler_registration counter_event_registration_;
-	dp::event_bus evt_bus;
-	void SetUp() override
-	{
-		counter_event_registration_ = evt_bus.register_handler<test_event_type>(&counter, &event_handler_counter::on_test_event);
-	}
+    event_handler_counter counter;
+    dp::handler_registration counter_event_registration_;
+    dp::event_bus evt_bus;
+    void SetUp() override
+    {
+        counter_event_registration_ = evt_bus.register_handler<test_event_type>(&counter, &event_handler_counter::on_test_event);
+    }
 
-	void TearDown() override
-	{
-		evt_bus.remove_handler(counter_event_registration_);
-	}
+    void TearDown() override
+    {
+        evt_bus.remove_handler(counter_event_registration_);
+    }
 };
 
 void free_function_callback(const test_event_type &type_event)
 {
-	std::cout << "Free function callback : " << type_event << "\n";
+    std::cout << "Free function callback : " << type_event << "\n";
 }
 
 TEST_F(EventBusTestFixture, LambdaRegistrationAndDeregistration)
 {
-	test_event_type test_event{ 1, "event message", 32.56 };
-	const auto lambda_one_reg = evt_bus.register_handler<test_event_type>([]() {std::cout << "Lambda 1\n"; });
-	const auto lambda_two_reg = evt_bus.register_handler<test_event_type>([&test_event](const test_event_type& evt)
-		{
-			EXPECT_EQ(evt.id, test_event.id);
-			EXPECT_EQ(evt.event_message, test_event.event_message);
-			EXPECT_EQ(evt.data_value, test_event.data_value);
-		});
+    test_event_type test_event{ 1, "event message", 32.56 };
+    const auto lambda_one_reg = evt_bus.register_handler<test_event_type>([]() {std::cout << "Lambda 1\n"; });
+    const auto lambda_two_reg = evt_bus.register_handler<test_event_type>([&test_event](const test_event_type& evt)
+        {
+            EXPECT_EQ(evt.id, test_event.id);
+            EXPECT_EQ(evt.event_message, test_event.event_message);
+            EXPECT_EQ(evt.data_value, test_event.data_value);
+        });
 
-	const auto lambda_three_reg = evt_bus.register_handler<test_event_type>([](test_event_type)
-		{
-			std::cout << "Lambda 3 take by copy.\n";
-		});
+    const auto lambda_three_reg = evt_bus.register_handler<test_event_type>([](test_event_type)
+        {
+            std::cout << "Lambda 3 take by copy.\n";
+        });
 
-	// should be 4 because we register a handler in the test fixture SetUp
-	ASSERT_EQ(evt_bus.handler_count(), 4);
-	evt_bus.fire_event(test_event);
-	EXPECT_EQ(counter.get_count(), 1);
-	evt_bus.fire_event(test_event);
-	EXPECT_EQ(counter.get_count(), 2);
+    // should be 4 because we register a handler in the test fixture SetUp
+    ASSERT_EQ(evt_bus.handler_count(), 4);
+    evt_bus.fire_event(test_event);
+    EXPECT_EQ(counter.get_count(), 1);
+    evt_bus.fire_event(test_event);
+    EXPECT_EQ(counter.get_count(), 2);
 
-	evt_bus.remove_handler(lambda_one_reg);
+    evt_bus.remove_handler(lambda_one_reg);
 
-	evt_bus.fire_event(test_event);
-	EXPECT_EQ(counter.get_count(), 3);
-	EXPECT_EQ(evt_bus.handler_count(), 3);
+    evt_bus.fire_event(test_event);
+    EXPECT_EQ(counter.get_count(), 3);
+    EXPECT_EQ(evt_bus.handler_count(), 3);
 
-	evt_bus.remove_handler(lambda_two_reg);
+    evt_bus.remove_handler(lambda_two_reg);
 
-	evt_bus.fire_event(test_event);
-	EXPECT_EQ(counter.get_count(), 4);
-	EXPECT_EQ(evt_bus.handler_count(), 2);
+    evt_bus.fire_event(test_event);
+    EXPECT_EQ(counter.get_count(), 4);
+    EXPECT_EQ(evt_bus.handler_count(), 2);
 
-	evt_bus.remove_handler(lambda_three_reg);
+    evt_bus.remove_handler(lambda_three_reg);
 
-	evt_bus.fire_event(test_event);
-	EXPECT_EQ(counter.get_count(), 5);
-	EXPECT_EQ(evt_bus.handler_count(), 1);
+    evt_bus.fire_event(test_event);
+    EXPECT_EQ(counter.get_count(), 5);
+    EXPECT_EQ(evt_bus.handler_count(), 1);
 }
 
 TEST_F(EventBusTestFixture, RegisterWhileDispatching) 
 {
-	struct nefarious_event_listener 
-	{
-		dp::event_bus* evt_bus;
-		void on_event(test_event_type evt) 
-		{
-			nefarious_event_listener listener;
-			listener.evt_bus = evt_bus;
-			if(evt_bus) 
-			{
-				evt_bus->register_handler<test_event_type>(&listener, &nefarious_event_listener::on_event);
-			}
-		}
-	};
+    struct nefarious_event_listener 
+    {
+        dp::event_bus* evt_bus;
+        void on_event(test_event_type evt) 
+        {
+            nefarious_event_listener listener;
+            listener.evt_bus = evt_bus;
+            if(evt_bus) 
+            {
+                auto _ = evt_bus->register_handler<test_event_type>(&listener, &nefarious_event_listener::on_event);
+            }
+        }
+    };
 
-	dp::handler_registration registration;
-	{
-		nefarious_event_listener listener;
-		listener.evt_bus = &evt_bus;
-		registration = evt_bus.register_handler<test_event_type>(&listener, &nefarious_event_listener::on_event);
-	}
+    dp::handler_registration registration;
+    {
+        nefarious_event_listener listener;
+        listener.evt_bus = &evt_bus;
+        registration = evt_bus.register_handler<test_event_type>(&listener, &nefarious_event_listener::on_event);
+    }
 
-	for(auto i = 0; i < 40; ++i) {
-		evt_bus.fire_event(test_event_type{2, "test event", 1.3});
-		std::cout << "Handler count: " << evt_bus.handler_count() << "\n";
-		// count should be 2 because we registered the first nefarious object and the 
-		// test fixture class is registered as well (the counter).
-		ASSERT_EQ(evt_bus.handler_count(), 2);
-	}
+    for(auto i = 0; i < 40; ++i) {
+        evt_bus.fire_event(test_event_type{2, "test event", 1.3});
+        std::cout << "Handler count: " << evt_bus.handler_count() << "\n";
+        // count should be 2 because we registered the first object and the 
+        // test fixture class is registered as well (the counter).
+        ASSERT_EQ(evt_bus.handler_count(), 2);
+    }
 
-	ASSERT_TRUE(evt_bus.remove_handler(registration));
+    ASSERT_TRUE(evt_bus.remove_handler(registration));
 }
 
 TEST_F(EventBusTestFixture, DeregisterWhileDispatching)
 {
 
-	struct deregister_while_dispatch_listener {
-		dp::event_bus *evt_bus{nullptr};
-		std::vector<dp::handler_registration> *registrations{nullptr};
-		void on_event(test_event_type)
-		{
-			if(evt_bus && registrations) {
-				std::for_each(registrations->begin(), registrations->end(), [&](auto reg) {
-					evt_bus->remove_handler(reg);
-				});
-			}
-		}
-	};
+    struct deregister_while_dispatch_listener {
+        dp::event_bus *evt_bus{nullptr};
+        std::vector<dp::handler_registration> *registrations{nullptr};
+        void on_event(test_event_type)
+        {
+            if(evt_bus && registrations) {
+                std::for_each(registrations->begin(), registrations->end(), [&](auto reg) {
+                    evt_bus->remove_handler(reg);
+                });
+            }
+        }
+    };
 
-	std::vector<dp::handler_registration> registrations;
-	std::vector<deregister_while_dispatch_listener> listeners;
-	for(auto i = 0; i < 20; ++i) {
-		deregister_while_dispatch_listener listener;
-		auto reg = evt_bus.register_handler<test_event_type>(&listener, &deregister_while_dispatch_listener::on_event);
-		listeners.emplace_back(listener);
-		registrations.emplace_back(reg);
-	}
-	
-	listeners[0].evt_bus = &evt_bus;
-	listeners[0].registrations = &registrations;
+    std::vector<dp::handler_registration> registrations;
+    std::vector<deregister_while_dispatch_listener> listeners;
+    for(auto i = 0; i < 20; ++i) {
+        deregister_while_dispatch_listener listener;
+        auto reg = evt_bus.register_handler<test_event_type>(&listener, &deregister_while_dispatch_listener::on_event);
+        listeners.emplace_back(listener);
+        registrations.emplace_back(reg);
+    }
+    
+    listeners[0].evt_bus = &evt_bus;
+    listeners[0].registrations = &registrations;
 
-	for(auto i = 0; i < 40; ++i) {
-		evt_bus.fire_event(test_event_type{3, "test event", 3.4});
-		std::cout << "Handler count: " << evt_bus.handler_count() << "\n";
-		// add 1 because of the test fixture.
-		EXPECT_EQ(evt_bus.handler_count(), listeners.size() + 1);
-	}
+    for(auto i = 0; i < 40; ++i) {
+        evt_bus.fire_event(test_event_type{3, "test event", 3.4});
+        std::cout << "Handler count: " << evt_bus.handler_count() << "\n";
+        // add 1 because of the test fixture.
+        EXPECT_EQ(evt_bus.handler_count(), listeners.size() + 1);
+    }
 
-	// remove all the registrations
-	for(auto reg: registrations) {
-		EXPECT_TRUE(evt_bus.remove_handler(reg));
-	}
+    // remove all the registrations
+    for(auto reg: registrations) {
+        EXPECT_TRUE(evt_bus.remove_handler(reg));
+    }
 
-	EXPECT_EQ(evt_bus.handler_count(), 1);
+    EXPECT_EQ(evt_bus.handler_count(), 1);
 }
 
 TEST(EventBus, MultiThreaded) {
-	class simple_listener {
-		int index_;
-	public:
-		simple_listener(int index) : index_(index){
+    class simple_listener {
+        int index_;
+    public:
+        simple_listener(int index) : index_(index){
 
-		}
-		void on_event(test_event_type) {
-			std::cout << "simple event " << index_ << "\n";
-		}
-	};
+        }
+        void on_event(test_event_type) {
+            std::cout << "simple event " << index_ << "\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+    };
 
-	dp::event_bus evt_bus;
-	simple_listener listener_one(1);
-	simple_listener listener_two(2);
-	
-	auto thread_one = std::thread([&](){
-		std::this_thread::sleep_for(std::chrono::milliseconds(400));
-		evt_bus.register_handler<test_event_type>(&listener_one, &simple_listener::on_event);
-	});
+    dp::event_bus evt_bus;
+    simple_listener listener_one(1);
+    simple_listener listener_two(2);
+    
+    auto thread_one = std::thread([&](){
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+        auto _ = evt_bus.register_handler<test_event_type>(&listener_one, &simple_listener::on_event);
+    });
 
-	auto thread_two = std::thread([&]() {
-		evt_bus.register_handler<test_event_type>(&listener_two, &simple_listener::on_event);
-		std::this_thread::sleep_for(std::chrono::milliseconds(400));
-		evt_bus.fire_event(test_event_type{3, "hello", 4.5});
-	});
+    auto thread_two = std::thread([&]() {
+        auto _ = evt_bus.register_handler<test_event_type>(&listener_two, &simple_listener::on_event);
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
+        evt_bus.fire_event(test_event_type{3, "hello", 4.5});
+    });
 
-	thread_one.join();
-	thread_two.join();
+    thread_one.join();
+    thread_two.join();
 
-	EXPECT_EQ(evt_bus.handler_count(), 2);
+    EXPECT_EQ(evt_bus.handler_count(), 2);
 }
